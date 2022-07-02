5bc0c9d72230d0a780f95327a69c3e75f2f06271
Brandon Buttlar <bjbzrc@mail.missouri.edu>
2022-07-01 20:32:47 -0500

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,12 @@
-for i in range(1,101):
-    fizz = 'Fizz' if i%3==0 else ''
-    buzz = 'Buzz' if i%5==0 else ''
-    print(f'{fizz}{buzz}' or i)
+# Final code for the IT 4320 FizzBuzz challenge
+# By Brandon Buttlar
+
+def main():
+    for i in range(1,101):
+        fizz = 'Fizz' if i % 3 == 0 else ''
+        buzz = 'Buzz' if i % 5 == 0 else ''
+        
+        print(f'{fizz}{buzz}' or i)
+        
+        
+main()
